2a132fa0390d6f2a030499689a81265410b40589
jwhaeke <jwhaeke@live.nl>
2018-12-21 15:18:03 +0100

    Added score text, counter, alert on hit and refresh when alert is clicked away

--- js/main.js
+++ js/main.js
@@ -1,23 +1,36 @@
 var bird;
 var pipes =[];
+var score = 0;
+var scoreArea;
+
 
 
 function setup() {
+    score = 0;
     createCanvas(1200, 600);
+
     bird = new Bird();
     pipes.push(new Pipe());
 }
 
 function draw() {
     background(0);
-    
+
+    scoreArea = document.getElementById("myScore");
+    scoreArea.innerHTML = "Your score: " + score;
+    console.log(score);
+
     for (var i = pipes.length-1; i >= 0; i--) {
         pipes[i].show();
         pipes[i].update();
         
         if (pipes[i].hits(bird)) {
-            console.log("HIT");
+            alert("You scored: " + score);
+            score = 0;
+            pipes.splice(0, pipes.length);
+            setup();
         }
+
 
         if (pipes[i].offscreen()) {
             pipes.splice(i,1);
@@ -28,9 +41,13 @@ function draw() {
     bird.show();
     
     // every 100 frames new pipe
-    if (frameCount % 100 == 0) {
+    if (frameCount % 40 == 0) { //If number of framecounts can be divided by 100 and the remained is 0 then result = True
         pipes.push(new Pipe());
 
+    }
+
+    if (frameCount % 1 == 0) {
+        score = score + 1;
     }
 
     
